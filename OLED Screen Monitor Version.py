a158d82dec6e5256061f626e2af82eeb3059feac
gadhagod <69025547+gadhagod@users.noreply.github.com>
2020-08-11 14:27:49 -0700

    Update OLED Screen Monitor Version.py

--- OLED Screen Monitor Version.py
+++ OLED Screen Monitor Version.py
@@ -1,4 +1,5 @@
-host
+#thanks so adafruit for base code
+import host
 import time
 import requests
 
@@ -10,116 +11,33 @@ from PIL import ImageDraw
 from PIL import ImageFont
 
 import subprocess
-url = "https://www.worldometers.info/coronavirus/"
-response = requests.get(url)
-htmlcodeline = response.text[:100001].split('\n')
 
-def cases():
-    location = htmlcodeline.index('<h1>Coronavirus Cases:</h1>')
-    cases = htmlcodeline[location + 2]
-    cases = cases.replace('<span style="color:#aaa">', '')
-    cases = cases.replace('</span>', '')
-    return cases
-
-def deaths():
-    location = htmlcodeline.index('<h1>Deaths:</h1>')
-    deaths = htmlcodeline[location + 2]
-    deaths = deaths.replace('<span>', '')
-    deaths = deaths.replace('</span>', '')
-    return deaths
-
-def recoveries():
-    location = htmlcodeline.index('<h1>Recovered:</h1>')
-    recoveries = htmlcodeline[location + 2]
-    recoveries = recoveries.replace('<span>', '')
-    recoveries = recoveries.replace('</span>', '')
-    return recoveries
-
-# Raspberry Pi pin configuration:
-RST = None     # on the PiOLED this pin isnt used
-# Note the following are only used with SPI:
+RST = None
 DC = 23
 SPI_PORT = 0
 SPI_DEVICE = 0
 
-# Beaglebone Black pin configuration:
-# RST = 'P9_12'
-# Note the following are only used with SPI:
-# DC = 'P9_15'
-# SPI_PORT = 1
-# SPI_DEVICE = 0
-
-# 128x32 display with hardware I2C:
 disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
-
-# 128x64 display with hardware I2C:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
-
-# Note you can change the I2C address by passing an i2c_address parameter like:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
-
-# Alternatively you can specify an explicit I2C bus number, for example
-# with the 128x32 display you would use:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=2)
-
-# 128x32 display with hardware SPI:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
-
-# 128x64 display with hardware SPI:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
-
-# Alternatively you can specify a software SPI implementation by providing
-# digital GPIO pin numbers for all the required display pins.  For example
-# on a Raspberry Pi with the 128x32 display you might use:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, sclk=18, din=25, cs=22)
-
-# Initialize library.
 disp.begin()
-
-# Clear display.
 disp.clear()
 disp.display()
 
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
 width = disp.width
 height = disp.height
 image = Image.new('1', (width, height))
-
-# Get drawing object to draw on image.
 draw = ImageDraw.Draw(image)
-
-# Draw a black filled box to clear the image.
 draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-# Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
 padding = -2
 top = padding
 bottom = height-padding
-# Move left to right keeping track of the current x position for drawing shapes.
 x = 0
 
-
-# Load default font.
 font = ImageFont.load_default()
 
-# Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
-# Some other nice fonts to try: http://www.dafont.com/bitmap.php
-# font = ImageFont.truetype('Minecraftia.ttf', 8)
-
 while True:
-
-    # Draw a black filled box to clear the image.
     draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-
-
-
-    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-
-
-    # Write two lines of text.
     deaths1 = "Deaths: " + str(deaths())
     cases1 = "Cases: " + str(cases())
     recoveries1 = "Recovered: " + str(recoveries())
@@ -128,7 +46,6 @@ while True:
     draw.text((x, top+10), cases1, font=font, fill=255)
     draw.text((x, top+20), recoveries1,  font=font, fill=255)
 
-    # Display image.
     disp.image(image)
     disp.display()
     time.sleep(.1)
