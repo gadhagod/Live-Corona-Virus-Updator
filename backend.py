151c29825fd1d736b8e6c3f093f75b6a5d1fd692
gadhagod <69025547+gadhagod@users.noreply.github.com>
2020-08-17 18:36:26 -0700

    Update backend.py

--- backend.py
+++ backend.py
@@ -12,19 +12,19 @@ class global_data:
     def __init__(self, code):
         self.code = code
 
-    def cases():
+    def cases(self):
         cases = self.code[self.code.index('<h1>Coronavirus Cases:</h1>') + 2]
         cases = cases.replace('<span style="color:#aaa">', '')
         cases = cases.replace('</span>', '')
         return cases
 
-    def deaths():
+    def deaths(self):
         deaths = self.code[self.code.index('<h1>Deaths:</h1>') + 2]
         deaths = deaths.replace('<span>', '')
         deaths = deaths.replace('</span>', '')
         return deaths
 
-    def recoveries():
+    def recoveries(self):
         deaths = self.code[self.code.index('<h1>Recovered:</h1>') + 2]
         recoveries = recoveries.replace('<span>', '')
         recoveries = recoveries.replace('</span>', '')
